125e6cc9147ee8459ceaefac6bc30b1b79cc2b21
RidiQirici <ridi.qirici@imb.al>
2015-07-08 16:40:31 +0200

    Ndryshime jar dhe java per hapjen e printimit

--- src/android/PrintPc700.java
+++ src/android/PrintPc700.java
@@ -1,10 +1,49 @@
 package imb.ridiqirici.plugin.cordova.pc700print;
 
 import org.apache.cordova.CordovaPlugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.IntentSender.SendIntentException;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.content.res.Resources.Theme;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
 import android.util.Log;
+import android.view.Display;
+
+import com.zkc.pc700.activity.MainActivity;
 import com.zkc.pc700.helper.PrinterClassSerialPort;
 
 public class PrintPc700 extends CordovaPlugin{
@@ -42,8 +81,14 @@ public class PrintPc700 extends CordovaPlugin{
         
 	public boolean printoTekstin(JSONArray arObj, int cutLines) {
 		String str = "Pershendetje";
+		boolean uPrintua = false;
 		try {
-			return printerClass.printText(str);
+			if (printerClass.open())
+			{
+				uPrintua = printerClass.printText(str);
+				printerClass.close();
+			}
+			return uPrintua;
 		} catch (Exception e) {
 			this.veprimiKryer = false;
         	this.mesazhi.error("Gabim gjate printimit te tekstit!");
