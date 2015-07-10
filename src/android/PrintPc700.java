6695d21e704defe91597937d9fb43ce6d093bc8b
RidiQirici <ridi.qirici@imb.al>
2015-07-10 16:26:18 +0200

    Ndryshime ne java

--- src/android/PrintPc700.java
+++ src/android/PrintPc700.java
@@ -99,7 +99,7 @@ public class PrintPc700 extends CordovaPlugin{
 				return this.veprimiKryer;
 			}*/
 			printerClass = new PrinterClassSerialPort();
-			s = printerClass.setSerialPortBaudrateProve();
+			s = printerClass.setSerialPortBaudrateProve(38400);
 			//this.veprimiKryer = printerClass.printText(str);
 			//this.veprimiKryer = printerClass.printText(str);
 			/*printerClass.setSerialPortBaudrate(38400);*/
